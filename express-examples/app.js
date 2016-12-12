8073027a1849978da24b136880688b30786ff4fd
Markus Opolka <markus@martialblog.de>
2016-12-12 13:00:39 +0100

    Add expressJS example

---
+++ express-examples/app.js
@@ -0,0 +1,18 @@
+var express = require('express')
+var app = express()
+
+app.route('/book')
+  .get(function (req, res) {
+    res.send('Get a random book')
+  })
+  .post(function (req, res) {
+    res.send('Add a book')
+  })
+  .put(function (req, res) {
+    res.send('Update the book')
+  })
+
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
